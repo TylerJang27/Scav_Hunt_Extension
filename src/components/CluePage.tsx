be4d01172a1bc830aa46ef9b715257d659b63344
Tyler Jang <tylerjang27@gmail.com>
2023-09-06 02:46:32 -0700

    refactor Beginning and CluePage

--- src/components/CluePage.tsx
+++ src/components/CluePage.tsx
@@ -21,12 +21,6 @@ import { nonNull } from "src/utils/helpers";
 
 import { Footer } from "./Footer";
 
-export interface BeginningPageProps {
-  title: React.ReactNode;
-  message: React.ReactNode;
-  backgroundURL: string;
-}
-
 export interface CluePageProps {
   huntName: string;
   encrypted: boolean;
@@ -171,41 +165,3 @@ export const CluePage = (props: CluePageProps) => {
     </>
   );
 };
-
-// TODO: TYLER REFACTOR TO MAKE THESE COMMON
-export const BeginningPage = (props: BeginningPageProps) => (
-  <>
-    <BackgroundWrapper backgroundURL={props.backgroundURL}>
-      <ThemeProvider theme={theme}>
-        <Container maxWidth="sm" sx={{ mt: 3, "&::after": { flex: "auto" } }}>
-          <Grid
-            container
-            spacing={2}
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Grid item xs={12}>
-              <Card sx={{ mt: 4, backgroundColor: "#333" }}>
-                <CardContent>
-                  <PageHeaderAndSubtitle header={props.title} />
-                  <Typography
-                    variant="body1"
-                    textAlign="center"
-                    color="white"
-                    mt={1}
-                  >
-                    {/* TODO: TYLER DO LINE REPLACEMENT */}
-                    {props.message}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12}>
-              <Footer />
-            </Grid>
-          </Grid>
-        </Container>
-      </ThemeProvider>
-    </BackgroundWrapper>
-  </>
-);
