d7f6a6af4a6bf225c5d1077f4083de45383fbadf
Tyler Jang <tylerjang27@gmail.com>
2023-09-25 02:03:54 -0700

    bump timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   // Look for test files in the "tests" directory, relative to this configuration file.
   testDir: "src/__tests__/integration_tests",
+  timeout: 60000,
 });
