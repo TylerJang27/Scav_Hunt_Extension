2317d583fafaa7014d1c5b881d5b1a86bae8cbeb
Tyler Jang <tylerjang27@gmail.com>
2023-10-06 00:31:50 -0700

    added footer reset hunt

--- src/components/reusable/Footer.tsx
+++ src/components/reusable/Footer.tsx
@@ -1,6 +1,9 @@
-import { Box, Link, Typography } from "@mui/material";
+import { Box, Button, Divider, Link, Stack, Typography } from "@mui/material";
 import { yellow } from "@mui/material/colors";
 import React from "react";
+import { logger } from "src/logger";
+import { resetStorage } from "src/providers/helpers";
+import { getURL } from "src/providers/runtime";
 
 export const Footer = () => (
   <>
@@ -9,7 +12,7 @@ export const Footer = () => (
         sx={{
           width: "100%",
           height: "auto",
-          backgroundColor: "#a8a8a850",
+          backgroundColor: "#989898d0",
           paddingTop: "1rem",
           paddingBottom: "1rem",
           position: "fixed",
@@ -17,15 +20,46 @@ export const Footer = () => (
           left: "0",
         }}
       >
-        <Typography variant="body1" textAlign="center">
-          {/* TODO: TYLER ADD OTHER LINKS AND THINGS */}
-          <Link
-            href="https://github.com/TylerJang27/Scav_Hunt_Extension"
+        <Stack
+          direction="row"
+          spacing={2}
+          divider={<Divider orientation="vertical" flexItem />}
+          sx={{ justifyContent: "center" }}
+        >
+          <Typography variant="body1" textAlign="center">
+            {/* TODO: TYLER ADD OTHER LINKS AND THINGS */}
+            <Link href={getURL("landing_page.html")} color={yellow[600]}>
+              Home
+            </Link>
+          </Typography>
+          <Typography variant="body1" textAlign="center">
+            {/* TODO: TYLER ADD OTHER LINKS AND THINGS */}
+            <Link
+              href="https://github.com/TylerJang27/Scav_Hunt_Extension"
+              color={yellow[600]}
+            >
+              GitHub
+            </Link>
+          </Typography>
+          <Typography
+            variant="body1"
+            textAlign="center"
+            role="button"
             color={yellow[600]}
+            onClick={() => {
+              const confirmBox = window.confirm(
+                "Do you really want to reset your progress?",
+              );
+              if (confirmBox === true) {
+                logger.info("Resetting hunt");
+                resetStorage(() => logger.info("Finished removing hunt"));
+              }
+            }}
+            sx={{ textDecoration: "underline" }}
           >
-            Scavenger Hunt Extension
-          </Link>
-        </Typography>
+            Reset Hunt
+          </Typography>
+        </Stack>
       </Box>
     </footer>
   </>
