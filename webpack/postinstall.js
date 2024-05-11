3f1a07c5a1dd55d5cd88e67d88fe4a854c689eeb
Tyler Jang <tylerjang27@gmail.com>
2024-05-10 21:44:57 -0700

    add default hunt to load

---
+++ webpack/postinstall.js
@@ -0,0 +1,22 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+console.log("hello!");
+
+async function copyIfNotExists(source, destination) {
+  try {
+    await fs.access(destination);
+    console.log("Destination file exists");
+  } catch {
+    console.log("Destination file does not exist, copying...");
+    await fs.copyFile(source, destination);
+    console.log("File copied");
+  }
+}
+
+// Usage
+const mocksDir = path.resolve(__dirname, "../src/providers/__mocks__");
+copyIfNotExists(
+  path.resolve(mocksDir, "example.ts"),
+  path.resolve(mocksDir, "hunt.ts"),
+);
